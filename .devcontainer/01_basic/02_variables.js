1c8cc22a9113c51afd5de8b5a086f42163a2962a
SWATANTRA26 <67919525+SWATANTRA26@users.noreply.github.com>
2024-05-28 17:10:54 +0000

    container config done

--- .devcontainer/01_basic/02_variables.js
+++ .devcontainer/01_basic/02_variables.js
@@ -3,8 +3,8 @@ const accountID = 144553
 let accountEmail ="swatantra@google.com"
 var accountPassword = "123"
 accountCity = "Bhopal"
-accountState = {};  // Output in console will be blank.
-let accState;       // Output will be undefined.
+accountState = {};      // Output in console will be blank.
+let accState;           // Output will be undefined.
 /*Note:
     In JS variable can be declared in 2 way by using let & var; 
     Scope {} curly bracess is also known as scope. So using var can create with scope. thus let is recommnded to use.
