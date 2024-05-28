4742312dd0634b0248ec8295fe3134868dc669a0
SWATANTRA26 <67919525+SWATANTRA26@users.noreply.github.com>
2024-05-28 13:26:57 +0000

    container config done

--- .devcontainer/01_basic/02_variables.js
+++ .devcontainer/01_basic/02_variables.js
@@ -1,3 +1,4 @@
-const accountID = 144553
+const accountID = 144553  //Variable defined as const can't be modified, if accountID is declared with other value compiler will give error.
 let accountEmail ="swatantra@google.com"
 var accountPassword = "123"
+console.log([accountID,accountEmail, accountPassword])
