7a8b5d7fd76cb4222be867370db7707f4c1552a6
SWATANTRA26 <67919525+SWATANTRA26@users.noreply.github.com>
2024-06-04 16:47:04 +0000

    container change done

---
+++ .devcontainer/01_basic/07_memory.js
@@ -0,0 +1,8 @@
+// Memory in JS 
+// Stack : All primitive, Will get copy of memory & Heap (Non-premitive) will get refrence of memory 
+let name1 = "SWATANTRA"
+let newName = name1;
+console.log(name1, newName)
+newName = 'swatantra'
+console.log(name1,newName)
+
