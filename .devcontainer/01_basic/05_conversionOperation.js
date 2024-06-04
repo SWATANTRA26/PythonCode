b69a95a181882b63e428fc0106466a6acf1996a8
SWATANTRA26 <67919525+SWATANTRA26@users.noreply.github.com>
2024-06-04 12:45:00 +0000

    container change done

--- .devcontainer/01_basic/05_conversionOperation.js
+++ .devcontainer/01_basic/05_conversionOperation.js
@@ -1,7 +1,7 @@
 /* In JS it is slightly issue in conversion 
     when we are taking data to front end of backend we don't know from where vlaue is coming 
     To convert values we can use Number, Boolean, String 
-    */
+*/
 let score = "33ab"
 console.log(typeof score)
 console.log(typeof(score))
@@ -15,3 +15,19 @@ let isLogin = 1
 let booleanIsLoggedIn = Boolean(isLogin)
 console.log(booleanIsLoggedIn)
 
+/*
+**********************Operations*****************************
+Addition, Subtraction 
++ - * **(power) /, %(reminder)
+*/
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Hello"
+let str2 = "Swatantra"
+let str3 = str1 + str2
+console.log(str3)
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+//https://tc39.es/ecma262/#sec-object-environment-records
