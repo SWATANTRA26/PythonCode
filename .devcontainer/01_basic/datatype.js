a508eca1b8ffe3baf9ac9fd315bd60aa63577633
SWATANTRA26 <67919525+SWATANTRA26@users.noreply.github.com>
2024-06-04 10:11:25 +0000

    datatypes

---
+++ .devcontainer/01_basic/datatype.js
@@ -0,0 +1,17 @@
+// 
+"use strict"; // treat all JS code as new version 
+
+//alert("Hello") // We are using node JS, not browser 
+console.log(3+3);
+console.log("Swatantra")
+
+//ecmascript //https://tc39.es & MDN
+let name = "Swatantra "     // String data type
+let age = 32                // number 
+let isLogin = false         // boolean DT
+                            // null is DT and standalon value '
+                            // undefined 
+                            // Symbol : To find unique 
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
