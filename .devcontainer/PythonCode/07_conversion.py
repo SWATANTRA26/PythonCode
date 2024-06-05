434ab58a432eae14b52b9a757180c16dc696c3cb
SWATANTRA26 <67919525+SWATANTRA26@users.noreply.github.com>
2024-06-05 14:03:38 +0000

    container change done

---
+++ .devcontainer/PythonCode/07_conversion.py
@@ -0,0 +1,13 @@
+#Conversion of Number system
+n1=int(input('Enter Your Number := '))
+print('Entered number is ={0}\t{1}'.format(n1,type(n1)))
+print("Decimal to Hexadecimal = ",hex(n1))
+print("Decimal to Octal = ",oct(n1))
+print("Decimal to Binary = ",bin(n1))
+
+#Lets enter an binary input
+#b1=int(input('Enter binary input = '))
+#b2=int(b1,2)
+#print(b2,type(b2))
+
+## Challenge is how to process directly an binary input ??
