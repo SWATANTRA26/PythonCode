070528c0d915365a218e8ffd580badced46c0f07
SWATANTRA26 <67919525+SWATANTRA26@users.noreply.github.com>
2024-06-05 12:45:28 +0000

    container change done

---
+++ .devcontainer/PythonCode/02_string.py
@@ -0,0 +1,29 @@
+# There are 6 type of sequences in python
+#What is sequence : It a group of element or items
+# In Python we have 6 type of sequence 
+# str, bytes, bytearray, list, tuple, range
+
+# Example of str 
+str = "Welcome"
+str ='Welcome'
+
+# str represents string datatype a string is enclosed in single quotes or double 
+
+str1 ='''This is a book on python which has all topics of python
+it is very usefull for new programmer'''
+
+str2 ="""This is a book on python which has all topics of python
+it is very usefull for new programmer"""
+
+# Str1 and str2 will have same results 
+print(str)
+print (str1)
+print (str2)
+print(str[0]) # first charector of variable str
+print(str1[0]) # first charector of variable str1
+# Lets print some range
+print(str[0:7])
+print(str1[0:7])
+print(str[-1]) # Display first charector from end =e
+print(str[-2]) # Display first charector from end =m
+print(str[-1:-7]) # Display first charector from end =m
