c5d994a6197044a147d102818aab8ed6e0d916df
SWATANTRA26 <67919525+SWATANTRA26@users.noreply.github.com>
2026-09-18 03:25:49 +0000

    commit

---
+++ .devcontainer/C#/_0000_Hello.cs
@@ -0,0 +1,7 @@
+using System;
+    Console.WriteLine("Hello, Welcome to C#!");//Used to print.
+    //Take Input from console and store in an variable.
+    int _input = Console.Read("Enter a number: ");
+    Console.Write(_input);
+    string input = Console.ReadLine(); //Convert the input string to an integer.
+    Console.WriteLine($"You entered: {input}");
